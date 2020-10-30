31520ebaf216d825d441afcd8a7c0572a671cc0a
bluebluepupu <309351223@qq.com>
2020-10-31 07:32:10 +0800

    调试Travis CI

--- tests/api/httpbin.py
+++ tests/api/httpbin.py
@@ -1,11 +1,23 @@
 from AATestingApi.basic_api import BasicApi
 
+# 这里都是接口的描述
+# 这里不需要写params、data、json这几个参数，basicApi中有封装注入这几个参数的方法
+
 
 class ApiHttpbinGet(BasicApi):
     url = "https://httpbin.org/get"
     method = "GET"
+    headers = {"accept": "application/json"}
+
 
 
 class ApiHttpbinPost(BasicApi):
     url = "https://httpbin.org/post"
     method = "POST"
+    headers = {"accept": "application/json"}
+
+
+class ApiHttpbinGetCookies(BasicApi):
+    url = "https://httpbin.org/cookies"
+    method = "GET"
+    headers = {"accept": "application/json"}
